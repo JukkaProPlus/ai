e08e80e14951bb5614c0d4e885ebfc2e4f4b6148
JukkaProPlus <jukka1993@foxmail.com>
2024-07-13 00:47:28 +0800

    ...

--- testVectorDB.py
+++ testVectorDB.py
@@ -4,3 +4,9 @@ from Tools import *
 dbConnector = MyVectorDBConnector(collection_name="demo_vec_rrf", embedding_fn=embedding_fn)
 result = dbConnector.search(query="坦克", top_n=5)
 print(result)
+print("*************************")
+for docs in result["documents"]:
+    for doc in docs:
+        print("")
+        print(doc)
+print("++++++++++++++++++++++++++++++")
