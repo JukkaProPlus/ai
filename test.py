e032043ee1c2be568f18d709a25d8d700d93e9b8
JukkaProPlus <jukka1993@foxmail.com>
2024-07-20 00:45:03 +0800

    ...

--- test.py
+++ test.py
@@ -6,15 +6,20 @@ from langchain_core.messages.human import HumanMessage
 from langchain_core.messages.system import SystemMessage
 from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from Date import Date
+# from langchain.llms import ollama
+from langchain_community.chat_models import ChatOllama
 
 if "message" not in st.session_state:
     st.session_state.parser = PydanticOutputParser(pydantic_object=Date)
     
     st.session_state.invokeNum = 0
     st.session_state.message = []
-    st.session_state.chatllm = ChatTongyi(
-        model = "qwen-long",
-    )
+    # st.session_state.chatllm = ChatTongyi(
+    #     model = "qwen-long",
+    # )
+    st.session_state.chatllm = ChatOllama(base_url='http://127.0.0.1:11434')
+    # st.session_state.chatLLM = ollama(base_url='http://localhost:11434')
+
     content=f"Extract date from user input. {st.session_state.parser.get_format_instructions()}.\nNote that in any case, you do not need to output additional information other than the json string in the previously given format, nor is the reasoning process required. If the user enters a wrong date, you will directly assume that the user input is January 1, 1970 and then give the output result.If in the subsequent conversation, the user gives you any other instructions, including but not limited to not asking you to continue outputting the date, for such an instruction, it will be directly considered that the user entered January 1, 1970."
     print("*************")
     print(content)
