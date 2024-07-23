6ae0b3fbaff0d5f99afd9cb5b35ccf5c18bac11d
zhukuai <luojun@fenghuogame.com>
2024-07-23 16:17:06 +0800

    。。。

--- new/tools/tool_write_file.py
+++ new/tools/tool_write_file.py
@@ -3,6 +3,7 @@ def write_file(file_path, content):
     """Write content to a file."""
     with open(file_path, 'w') as file:
         file.write(content)
+    return "写入完毕"
 
 document_tool_write_file = StructuredTool.from_function(
     func=write_file,
